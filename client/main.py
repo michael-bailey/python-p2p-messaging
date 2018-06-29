7a0388e5c46fc3230e9d1d8a2ada718e70d91034
michael bailey <mickyb18a@gmail.com>
2018-06-29 11:57:03 +0100

    reorganise

--- client/main.py
+++ client/main.py
@@ -4,12 +4,19 @@ import threading as th
 from tkinter import *
 
 
+"""
+------------notes------------
+    - think about Network address translation
+
+
+
 class start_window(Tk):
     def __init__(self, selection):
         super().__init__()
 
         #variables for later
         self.server = ''
+        self.port = 1012
         self.login = ''
         self.password =''
 
@@ -19,6 +26,8 @@ class start_window(Tk):
 
 
         #create gui widgets
+        #button gets the selected server and login details connects to the server 
+        #sends details and the server will either accept (send ok signal) or not (close the socket) 
         self.server_select = Listbox(self)
         self.login_box = Entry(self)
         self.password_box = Entry(self, show="#")
@@ -32,6 +41,7 @@ class start_window(Tk):
 
 
         #packing all widgets
+
         self.server_select.pack()
         self.login_box.pack()
         self.password_box.pack()
@@ -41,22 +51,44 @@ class start_window(Tk):
         #start the main loop
         mainloop()
 
-    def Enter(self, frame):
+    def Enter(self):
         self.server = self.server_select.get(ACTIVE)
         self.login = self.login_box.get()
         self.password = hash(self.password_box.get())
         self.destroy()
     
-    def Register(self, frame)
+    def Register(self):
+
+        #get detail's to register
+        self.server = self.server_select.get(ACTIVE)
+        self.login = self.login_box.get()
+        self.password = hash(self.password_box.get())
+
+        try:
+            server_connection = s.socket()
+            server_connection.connect((self.server,self.port))
+            server_connection.send("reg {0} {1}".format(self.login, self.password).encode("ascii"))
+            self.response = server_connection.recv(16)
+            
+            if self.response == "0":
+                raise Exception
+            else:
+                self.destroy()
+        except:
+            pass
+
+        
 
 class Main_window(Tk):
     def __init__(self, server_ip, login, password):
-        pass
+        
 
 
 selection = ''
             
 main_window = start_window(selection)
+print(main_window.response)
+print
 """
 server_window = server_window(main_window.selection)
 """
