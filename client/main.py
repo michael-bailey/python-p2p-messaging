1c389a1d04019e5336ea871713db09daeaa83c09
michael bailey <mickyb18a@gmail.com>
2018-07-11 17:21:29 +0100

    started work on the thread functions

--- client/main.py
+++ client/main.py
@@ -35,30 +35,22 @@ class scrollListBox(tk.Frame):
         self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-    #internal functions
-    def update(self, event):
-        self.state = self.listbox.get(tk.ACTIVE)
-        try:
-            self.on_click()
-        except:
-            pass
-
 
 
 
 class messageFrame(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, command=None):
         super().__init__(parent)
-
 
 
         #parent reference
         self.parent = parent
+        self.command = command
 
         #creating widget definitions
         self.messageView = scrollListBox(self)
         self.entryBox = tk.Entry(self)
-        self.enterButton = tk.Button(self, text="enter")
+        self.enterButton = tk.Button(self, text="enter", command=self.update)
 
         #defining attributes
 
@@ -69,15 +61,19 @@ class messageFrame(tk.Frame):
         self.messageView.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.entryBox.pack(fill=tk.X, expand=1, side=tk.LEFT )
         self.enterButton.pack(side=tk.RIGHT)
+    
 
 
 class application(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        #defining variables
+        self.active_client
+
         #creating widget definitions
         self.splitPane = tk.PanedWindow(self, handlepad=16, showhandle=True)
-        self.pane1_contacts = scrollListBox(self)
+        self.pane1_contacts = scrollListBox(self).listbox.bind("<Button-1>", func=self.cange_connection)
         self.pane2_messages = messageFrame(self)
 
         self.splitPane.add(self.pane1_contacts)
@@ -90,12 +86,26 @@ class application(tk.Tk):
         self.connections = th.Thread(target=self.connection_handler)
         self.sync = th.Thread(target=self.server_sync)
 
+        #creating bindings to functions
+
         tk.mainloop()
 
+    #binding fumctions
+    def send_message(self):
+        self.pane2_messages.messageView.listbox.insert(tk.END, )
+
+    def change_active_client(self):
+        pass
+
+    #thread functions
     def connection_handler(self):
-        pass
+        sock = s.socket()
+        sock.listen(5)
+
+        
+
     def server_sync(self):
-        pass
+        t.sleep(1)
 
 
         
