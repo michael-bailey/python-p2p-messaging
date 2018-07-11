c4a1630a7d181204211d080991bb3095b1117bd9
michael bailey <mickyb18a@gmail.com>
2018-07-11 13:41:34 +0100

    finishing gui design

--- client/main.py
+++ client/main.py
@@ -1,8 +1,9 @@
-import socket as s
 import threading as th
 import tkinter as tk
-import os
+import tkinter.font as font
+import socket as s
 import time as t
+import os
 
 """
 ------------notes------------
@@ -10,12 +11,14 @@ import time as t
 
 """
 
-
+#default_font = font.Font(family="microsoft sans serif", size="32" )
 
 #creating a composite widget that combines the list box and a scroll bar
 class scrollListBox(tk.Frame):
     def __init__(self, parent, on_click=None):
         super().__init__(parent)
+
+        self.font=font
 
         #parent object reference's
         self.parent = parent
@@ -31,16 +34,20 @@ class scrollListBox(tk.Frame):
         self.listbox.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.listbox.yview)
 
-        self.listbox.bind("<Button-1>", on_click)
+        self.listbox.bind("<Button-1>", self.update)
 
         #packing widgets
         self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        #internal functions
-        def update(self):
-            self.state = self.listbox.get()
+    #internal functions
+    def update(self, event):
+        self.state = self.listbox.get(tk.ACTIVE)
+        try:
             self.on_click()
+        except:
+            pass
+
 
 
 
@@ -48,27 +55,51 @@ class messageFrame(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.font=font
+
         #parent reference
         self.parent = parent
 
         #creating widget definitions
         self.messageView = scrollListBox(self)
         self.entryBox = tk.Entry(self)
-        self.enterButton = tk.Button(self)
+        self.enterButton = tk.Button(self, text="enter")
 
         #defining attributes
 
         #set bindings and events
 
-        #packing widgets
 
-"""
+        #packing widgets
+        self.messageView.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.entryBox.pack(fill=tk.X, expand=1, side=tk.LEFT )
+        self.enterButton.pack(side=tk.RIGHT)
+
+
 class application(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        self.font=font
+
+        #creating widget definitions
         self.splitPane = tk.PanedWindow(self)
-        self.
-"""
+        self.pane1_contacts = scrollListBox(self)
+        self.pane2_messages = messageFrame(self)
+
+        self.splitPane.add(self.pane1_contacts)
+        self.splitPane.add(self.pane2_messages)
+
+        #packing widgets
+        self.splitPane.pack(fill=tk.BOTH,expand=1)
+
+
+
+
+        tk.mainloop()
+
+    def connection_handler(self):
+
         
 
+a = application()
