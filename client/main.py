2e19e1d6dd491f046ab3be177acc8ed8fb68b425
michael bailey <mickyb18a@gmail.com>
2018-07-12 09:51:37 +0100

    started reworking to fit proper OOP standards by hiding infomation

--- client/main.py
+++ client/main.py
@@ -1,6 +1,7 @@
 import threading as th
 import tkinter as tk
 import socket as s
+import json as js
 import time as t
 import os
 
@@ -15,37 +16,26 @@ class scrollListBox(tk.Frame):
     def __init__(self, parent, on_click=None):
         super().__init__(parent)
 
-        #parent object reference's
-        self.parent = parent
-        self.on_click = on_click
-
         #creating widget definitions
         self.listbox = tk.Listbox(self)
         self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
-
-        #defining attributes
 
         #set bindings and events
         self.listbox.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.listbox.yview)
 
-        self.listbox.bind("<Button-1>", self.update)
-
         #packing widgets
         self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-
-
-
 class messageFrame(tk.Frame):
-    def __init__(self, parent, command=None):
+    def __init__(self, parent, command=None, on_click=None):
         super().__init__(parent)
 
 
         #parent reference
-        self.parent = parent
-        self.command = command
+        self.ArgCommand = command
+        self.ArgOn_click = on_click
 
         #creating widget definitions
         self.messageView = scrollListBox(self)
@@ -54,30 +44,65 @@ class messageFrame(tk.Frame):
 
         #defining attributes
 
-        #set bindings and events
-
-
         #packing widgets
         self.messageView.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.entryBox.pack(fill=tk.X, expand=1, side=tk.LEFT )
         self.enterButton.pack(side=tk.RIGHT)
-    
 
+        #defining command functions
+    def command(self):
+        try:
+            self.ArgCommand()
+        except:
+            print("ArgCommand failed")
+            pass
+    
+    def on_click(self):
+        try:
+            self.ArgOn_click()
+        except:
+            print("ArgCommand failed")
+            pass
+    
+class serverSelectWindow(tk.Toplevel):
+    def __init__(self, on_click=None):
+        super().__init__()
+        self.selector = scrollListBox(self)
+
+        self.ArgOn_click = on_click
+    
+    def on_click(self):
+        try:
+            self.ArgOn_click()
+        except:
+            print("click failed")
+            pass
 
 class application(tk.Tk):
     def __init__(self):
         super().__init__()
 
         #defining variables
-        self.active_client
+        self.active_client = 0
+        self.client_details = {}
+
+        #defining other toplevel widgets
+        self.server_selector = serverSelectWindow
 
         #creating widget definitions
         self.splitPane = tk.PanedWindow(self, handlepad=16, showhandle=True)
-        self.pane1_contacts = scrollListBox(self).listbox.bind("<Button-1>", func=self.cange_connection)
+        self.pane1_contacts = scrollListBox(self)
         self.pane2_messages = messageFrame(self)
 
         self.splitPane.add(self.pane1_contacts)
         self.splitPane.add(self.pane2_messages)
+
+        #setting up bindings
+        self.pane1_contacts.listbox.bind("<Button-1>", func=self.change_active_client)
+
+        self.pane2_messages.enterButton["command"]=self.send_message
+
+        
 
         #packing widgets
         self.splitPane.pack(fill=tk.BOTH,expand=1)
@@ -85,14 +110,15 @@ class application(tk.Tk):
         #create handler threads
         self.connections = th.Thread(target=self.connection_handler)
         self.sync = th.Thread(target=self.server_sync)
-
-        #creating bindings to functions
-
+        
         tk.mainloop()
 
     #binding fumctions
     def send_message(self):
-        self.pane2_messages.messageView.listbox.insert(tk.END, )
+
+        
+
+        self.pane2_messages.messageView.listbox.insert(tk.END, self.pane2_messages.entryBox.get())
 
     def change_active_client(self):
         pass
@@ -102,12 +128,12 @@ class application(tk.Tk):
         sock = s.socket()
         sock.listen(5)
 
-        
+        sock.accept()
 
     def server_sync(self):
+
+
         t.sleep(1)
 
-
-        
 
 a = application()
